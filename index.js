8071cf2780d63395d735bdfc24b5474d63f3b839
unknown <ibrahimkhalil.mine@gmail.com>
2022-05-07 11:28:11 +0600

    mongo bd connected

--- index.js
+++ index.js
@@ -2,8 +2,30 @@ const express = require("express")
 const app = express()
 const port = process.env.PORT || 5000
 const cors = require("cors")
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+require('dotenv').config()
 app.use(cors())
 app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dnjxk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect()
+        const mobileCollection = client.db("MobileLand").collection("Mobile");
+        console.log("db connected")
+    }
+    finally {
+        await client.close()
+    }
+}
+run().catch(console.dir)
+
 
 app.get("/", (req, res) => {
     res.send("look mama Mobile Land server")
